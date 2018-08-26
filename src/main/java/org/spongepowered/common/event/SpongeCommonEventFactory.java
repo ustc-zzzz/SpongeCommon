ac8be26aebd9e297fcafc9e9cb908a04f51f3e12
Yanbing Zhao <zzzz@mail.ustc.edu.cn>
2018-08-26 20:56:07 +0800

    Implement ChangeEntityEquipmentEvents

--- src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
+++ src/main/java/org/spongepowered/common/event/SpongeCommonEventFactory.java
@@ -67,6 +67,7 @@ import org.spongepowered.api.entity.Entity;
 import org.spongepowered.api.entity.Item;
 import org.spongepowered.api.entity.Transform;
 import org.spongepowered.api.entity.living.Agent;
+import org.spongepowered.api.entity.living.Human;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.User;
@@ -82,6 +83,7 @@ import org.spongepowered.api.event.cause.Cause;
 import org.spongepowered.api.event.cause.EventContext;
 import org.spongepowered.api.event.cause.EventContextKeys;
 import org.spongepowered.api.event.cause.entity.spawn.SpawnTypes;
+import org.spongepowered.api.event.entity.ChangeEntityEquipmentEvent;
 import org.spongepowered.api.event.entity.CollideEntityEvent;
 import org.spongepowered.api.event.entity.DestructEntityEvent;
 import org.spongepowered.api.event.entity.InteractEntityEvent;
@@ -133,6 +135,7 @@ import org.spongepowered.common.interfaces.entity.player.IMixinEntityPlayerMP;
 import org.spongepowered.common.interfaces.entity.player.IMixinInventoryPlayer;
 import org.spongepowered.common.interfaces.world.IMixinWorldServer;
 import org.spongepowered.common.item.inventory.adapter.InventoryAdapter;
+import org.spongepowered.common.item.inventory.adapter.impl.slots.SlotAdapter;
 import org.spongepowered.common.item.inventory.custom.CustomInventory;
 import org.spongepowered.common.item.inventory.util.ContainerUtil;
 import org.spongepowered.common.item.inventory.util.InventoryUtil;
@@ -1429,5 +1432,23 @@ public class SpongeCommonEventFactory {
     public static void callPostPlayerRespawnEvent(EntityPlayerMP playerMP, boolean conqueredEnd) {
         // We overwrite this method in SpongeForge, in order to fire
         // Forge's PlayerRespawnEvent
+    }
+
+    public static ChangeEntityEquipmentEvent callChangeEntityEquipmentEvent(EntityLivingBase entity, ItemStackSnapshot before, ItemStackSnapshot after, SlotAdapter slot) {
+        ChangeEntityEquipmentEvent event;
+        Cause currentCause = Sponge.getCauseStackManager().getCurrentCause();
+        Transaction<ItemStackSnapshot> transaction = new Transaction<>(before, after);
+        if (entity instanceof EntityPlayerMP) {
+            Player player = EntityUtil.toPlayer((EntityPlayerMP) entity);
+            event = SpongeEventFactory.createChangeEntityEquipmentEventTargetPlayer(currentCause, player, slot, transaction);
+        } else if (entity instanceof Human) {
+            Human humanoid = (Human) entity;
+            event = SpongeEventFactory.createChangeEntityEquipmentEventTargetHumanoid(currentCause, humanoid, slot, transaction);
+        } else {
+            Living living = EntityUtil.fromNativeToLiving(entity);
+            event = SpongeEventFactory.createChangeEntityEquipmentEventTargetLiving(currentCause, living, slot, transaction);
+        }
+        SpongeImpl.postEvent(event);
+        return event;
     }
 }
